acf9ffcef6931a60ccf2baa675fdad2f42f57e3a
Ethan Snead <115378751+theBaconn@users.noreply.github.com>
2025-11-21 14:52:39 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+  
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+import Nav from './nav'
+export default function Home() {
+    return (
+    <div className='min-h-screen bg-gray-50 bg-cover'>
+      <Nav/>
+    </div>
+  )
+}
